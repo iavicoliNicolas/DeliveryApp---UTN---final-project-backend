901a78e233931dda75b8f4299d9ed629c07ebd7c
Bstiasss <53101755+Bstiasss@users.noreply.github.com>
2026-05-08 20:11:36 -0300

    Update StoreController.java

--- src/main/java/com/deliveryapp/backend/store/controller/StoreController.java
+++ src/main/java/com/deliveryapp/backend/store/controller/StoreController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/stores")
+@RequestMapping("/api/v1/stores")
 @RequiredArgsConstructor
 public class StoreController {
     private final IStoreService storeService;
