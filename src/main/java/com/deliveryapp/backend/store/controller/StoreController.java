ee073b8972d41e5e737cb3726f8f29d3d2d4596d
TomasAmatoLebron <amato2807@gmail.com>
2026-05-08 18:02:18 -0300

    Created StoreController

---
+++ src/main/java/com/deliveryapp/backend/store/controller/StoreController.java
@@ -0,0 +1,54 @@
+package com.deliveryapp.backend.store.controller;
+
+import com.deliveryapp.backend.store.dto.StoreRequestDTO;
+import com.deliveryapp.backend.store.dto.StoreResponseDTO;
+import com.deliveryapp.backend.store.exception.StoreNotFoundException;
+import com.deliveryapp.backend.store.service.IStoreService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/stores")
+@RequiredArgsConstructor
+public class StoreController {
+    private final IStoreService storeService;
+
+    @GetMapping
+    public ResponseEntity<List<StoreResponseDTO>> findAllStores(){
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(storeService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StoreResponseDTO> findStoreById(@PathVariable Long id){
+        StoreResponseDTO storeResponseDTO = storeService.findById(id).orElseThrow(
+                () -> new StoreNotFoundException(id)
+        );
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(storeResponseDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<StoreResponseDTO> create(@Valid @RequestBody StoreRequestDTO storeRequestDTO){
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(storeService.save(storeRequestDTO));
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StoreResponseDTO> update(@Valid @PathVariable Long id, @RequestBody StoreRequestDTO storeRequestDTO){
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(storeService.update(id,storeRequestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        storeService.deleteById(id);
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body("Store with id " + id + " deleted");
+    }
+
+
+
+
+}
