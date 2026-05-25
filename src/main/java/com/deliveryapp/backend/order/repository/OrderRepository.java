8deada556ca9b570113e23063ce0069736b68aaf
lucas <lomuoio@gmail.com>
2026-05-25 12:07:01 -0300

    fix: duplicated notation

--- src/main/java/com/deliveryapp/backend/order/repository/OrderRepository.java
+++ src/main/java/com/deliveryapp/backend/order/repository/OrderRepository.java
@@ -4,8 +4,10 @@ import com.deliveryapp.backend.order.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-@Repository
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByStoreId(Long storeId);
 }
