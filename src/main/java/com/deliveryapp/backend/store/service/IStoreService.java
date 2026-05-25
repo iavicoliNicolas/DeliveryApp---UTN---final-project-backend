6ee7e165af26c605e1291ee44aff9f8c92b129b2
TomasAmatoLebron <amato2807@gmail.com>
2026-05-25 18:44:19 -0300

    Added Pagination findAll method to StoreService + StoreSearchMissingLocationException created

--- src/main/java/com/deliveryapp/backend/store/service/IStoreService.java
+++ src/main/java/com/deliveryapp/backend/store/service/IStoreService.java
@@ -1,12 +1,16 @@
 package com.deliveryapp.backend.store.service;
 
+import com.deliveryapp.backend.common.pagination.PaginationQuery;
+import com.deliveryapp.backend.common.pagination.PaginationResult;
 import com.deliveryapp.backend.store.dto.StoreRequestDTO;
 import com.deliveryapp.backend.store.dto.StoreResponseDTO;
+import com.deliveryapp.backend.store.filter.StoreFilter;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IStoreService {
+    PaginationResult<StoreResponseDTO> findAll(PaginationQuery paginationQuery, StoreFilter filter);
     List<StoreResponseDTO> findAll();
     StoreResponseDTO save(StoreRequestDTO storeRequestDTO);
     Optional<StoreResponseDTO> findById(Long id);
