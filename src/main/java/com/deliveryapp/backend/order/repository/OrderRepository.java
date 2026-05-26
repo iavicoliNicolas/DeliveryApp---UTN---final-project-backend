6807d9f5aad0230a51efe783cb6f7d9f5cd931f7
TomasAmatoLebron <amato2807@gmail.com>
2026-05-26 15:34:39 -0300

    Quickfix: Import List interface at OrderRepository + deleted duplicated annotation

--- src/main/java/com/deliveryapp/backend/order/repository/OrderRepository.java
+++ src/main/java/com/deliveryapp/backend/order/repository/OrderRepository.java
@@ -5,7 +5,8 @@ import com.deliveryapp.backend.order.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByStoreId(Long storeId);
