312f6395254fce70fbb1154331b84f8ac21f34c1
TomasAmatoLebron <amato2807@gmail.com>
2026-06-16 17:22:36 -0300

    UserService updated

--- src/main/java/com/deliveryapp/backend/user/repository/UserRepository.java
+++ src/main/java/com/deliveryapp/backend/user/repository/UserRepository.java
@@ -3,11 +3,13 @@ package com.deliveryapp.backend.user.repository;
 import com.deliveryapp.backend.user.enums.ERole;
 import com.deliveryapp.backend.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
     List<User> findAllByRole(ERole role);
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
--- src/main/java/com/deliveryapp/backend/user/service/UserService.java
+++ src/main/java/com/deliveryapp/backend/user/service/UserService.java
@@ -1,15 +1,25 @@
 package com.deliveryapp.backend.user.service;
 
+import com.deliveryapp.backend.common.pagination.PaginationQuery;
+import com.deliveryapp.backend.common.pagination.PaginationResult;
+import com.deliveryapp.backend.product.exception.InvalidParameterSortByException;
 import com.deliveryapp.backend.user.dto.UserRequestDTO;
 import com.deliveryapp.backend.user.dto.UserResponseDTO;
 import com.deliveryapp.backend.user.dto.UserUpdateRequestDTO;
 import com.deliveryapp.backend.user.enums.ERole;
 import com.deliveryapp.backend.user.exception.UserNotFoundException;
+import com.deliveryapp.backend.user.filter.UserFilter;
 import com.deliveryapp.backend.user.mapper.UserMapper;
 import com.deliveryapp.backend.user.model.User;
 import com.deliveryapp.backend.user.repository.UserRepository;
+import com.deliveryapp.backend.user.specification.UserSpecification;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,12 +32,62 @@ public class UserService implements IUserService {
     // private final AuthFacadeService authFacadeService;
     //private final PasswordEncoder passwordEncoder;
 
+    @Transactional(readOnly = true)
     @Override
-    public List<UserResponseDTO> findAll() {
-        return userRepository.findAll()
-                .stream()
-                .map(userMapper::toResponse)
-                .toList();
+    public PaginationResult<UserResponseDTO> findAll(
+            PaginationQuery paginationQuery,
+            UserFilter userFilter) {
+
+        if (!(paginationQuery.getDirection().equalsIgnoreCase("asc")
+                || paginationQuery.getDirection().equalsIgnoreCase("desc"))) {
+
+            throw new InvalidParameterSortByException(
+                    "Parametro direction solo acepta valores: asc , desc"
+            );
+        }
+
+        if (!(paginationQuery.getSortBy().equalsIgnoreCase("id")
+                || paginationQuery.getSortBy().equalsIgnoreCase("name")
+                || paginationQuery.getSortBy().equalsIgnoreCase("lastName")
+                || paginationQuery.getSortBy().equalsIgnoreCase("email")
+                || paginationQuery.getSortBy().equalsIgnoreCase("role"))) {
+
+            throw new InvalidParameterSortByException(
+                    "Parametro sortBy solo acepta valores: id , name, lastName, email, role"
+            );
+        }
+
+        PageRequest pageRequest = PageRequest.of(
+                paginationQuery.getPage(),
+                paginationQuery.getSize(),
+                Sort.by(
+                        Sort.Direction.fromString(
+                                paginationQuery.getDirection()
+                        ),
+                        paginationQuery.getSortBy()
+                )
+        );
+
+        Specification<User> specification = Specification.allOf(
+                UserSpecification.byId(userFilter.getId())
+                        .and(UserSpecification.byName(userFilter.getName())
+                                .and(UserSpecification.byLastName(userFilter.getLastName())
+                                        .and(UserSpecification.byEmail(userFilter.getEmail())
+                                                .and(UserSpecification.byRole(userFilter.getRole()))))
+                        ));
+
+        Page<User> page = userRepository.findAll(specification, pageRequest);
+
+        return new PaginationResult<>(
+                page.getContent()
+                        .stream()
+                        .map(userMapper::toResponse)
+                        .toList(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalPages(),
+                page.getTotalElements()
+        );
     }
 
     @Override
